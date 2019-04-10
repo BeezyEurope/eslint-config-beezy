57fea8b79077f0b9d2268d53a08498068eb8f03c
Mario Terron <marioterron157@gmail.com>
2019-04-10 15:22:32 +0200

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,14 +9,17 @@ module.exports = {
 	"parser": "babel-eslint",
 	"rules": {
 		"prettier/prettier": "warn",
-		"no-return-assign": "warn",
+		"no-return-assign": "error",
+		"no-else-return": "warn",
 		"jsx-a11y/click-events-have-key-events": "warn",
 		"jsx-a11y/label-has-for": "warn",
 		"jsx-a11y/mouse-events-have-key-events": "warn",
 		"jsx-a11y/no-noninteractive-element-interactions": "warn",
-		"react/button-has-type": "warn",
-		"react/destructuring-assignment": "warn",
+		"react/button-has-type": "error",
+		"react/destructuring-assignment": "off",
 		"react/jsx-filename-extension": "off",
-		"react/require-default-props": "warn"
+		"react/require-default-props": "warn",
+		"react/sort-comp": "warn",
+		"react/jsx-curly-brace-presence": "warn"
 	}
 }
