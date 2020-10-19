6103c76bc196763abd4a81ff331221c91ff742fa
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-10-19 12:34:07 +0200

    Update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,5 @@
 module.export = {
   arrowParens: "avoid",
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
   printWidth: 80,
-  semi: true,
   singleQuote: true,
-  tabWidth: 2,
-  trailingComma: "es5",
-  useTabs: false,
 };
